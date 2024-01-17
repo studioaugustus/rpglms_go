155d07d97393a2f1b921ba8154bc7723231514bf
simmonsdillon <dillonaugustussimmons@gmail.com>
2024-01-17 11:51:38 -0600

    init a

--- main.go
+++ main.go
@@ -1,15 +1,15 @@
-package rpglms_go
+package main
 
 import (
 	"log"
 	"net/http"
 
-	"github.com/studioaugustus/rpglms_go/backend/internal/app"
+	"backend/internal/app"
 )
 
 func main() {
     handler := app.NewHandler()
-    http.HandleFunc("/", handler.ServeHTTP)
+    http.Handle("/", handler)
 
     log.Println("Starting server on :8080")
     log.Fatal(http.ListenAndServe(":8080", nil))
